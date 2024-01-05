7655e8553a12d24ceac2b1c6907727f1067f43cf
Lilian Mahut <lilian.mahut@pm.me>
2024-01-05 17:09:25 +0100

    add old project & up release

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularPokedex
+# @ngular Pokedex
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.0.8.
 
@@ -12,7 +12,7 @@ Run `ng generate component component-name` to generate a new component. You can 
 
 ## Build
 
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
+Run `ng build` to build the PokeDex. The build artifacts will be stored in the `dist/` directory.
 
 ## Running unit tests
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { POKEMONS } from "./mock-pokemon-list";
+import { Pokemon } from "./pokemon";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: [],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  title = 'angular_pokedex';
+export class AppComponent implements OnInit {
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon;
+
+  ngOnInit() {
+    console.table(this.pokemonList);
+  }
+
+  selectPokemon(pokemonId: string) {
+    const id = +pokemonId;
+    const pokemon: Pokemon | undefined = this.pokemonList.find(
+      (pokemon) => pokemon.id == +pokemonId
+    );
+    console.log(`Vous avez cliqué sur le pokémon ${this.pokemonList[id].name}`);
+  }
 }
