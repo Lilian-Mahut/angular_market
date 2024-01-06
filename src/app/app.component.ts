054eccfcb3e72955ec520614bec7096db5e59ce2
Lilian Mahut <lilian.mahut@pm.me>
2024-01-06 17:28:43 +0100

    mise en forme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { POKEMONS } from "./mock-pokemon-list";
 import { Pokemon } from "./pokemon";
 
