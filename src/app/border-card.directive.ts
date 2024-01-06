a878483cbad3af784f0ef6c2f4e3f56ad37b5913
Lilian Mahut <lilian.mahut@pm.me>
2024-01-06 21:25:50 +0100

    add directives

--- src/app/border-card.directive.ts
+++ src/app/border-card.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, Host, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostListener, Input } from "@angular/core";
 
 @Directive({
   selector: "[pkmnBorderCard]",
@@ -7,14 +7,21 @@ export class BorderCardDirective {
   constructor(private el: ElementRef) {
     this.setBorder("#cecacaaa");
     this.setBackgroundColor("#ede9c4");
+    this.setTransform("scale(1)");
   }
+  @Input("pkmnBorderCard") borderColor: string;
+  @Input("pkmnBbgColor") backgroundColor: string;
+  @Input("pkmnTranformScale") transformScale: string;
+
   @HostListener("mouseenter") onMouseEnter() {
-    this.setBorder("#009688");
-    this.setBackgroundColor("#f7f4e0");
+    this.setBorder(this.borderColor || "#009688");
+    this.setBackgroundColor(this.backgroundColor || "#f7f4e0");
+    this.setTransform(this.transformScale || "scale(1.07)");
   }
   @HostListener("mouseleave") onMouseLeave() {
     this.setBorder("#cecacaaa");
     this.setBackgroundColor("#ede9c4");
+    this.setTransform("scale(1)");
   }
 
   setBorder(color: string) {
@@ -22,5 +29,8 @@ export class BorderCardDirective {
   }
   setBackgroundColor(color: string) {
     this.el.nativeElement.style.background = `${color}`;
+  }
+  setTransform(scale: string) {
+    this.el.nativeElement.style.transform = `${scale}`;
   }
 }
