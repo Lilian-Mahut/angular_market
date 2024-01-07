42c2c915f1412ee7b0accda945b15569816250a7
Lilian Mahut <lilian.mahut@pm.me>
2024-01-07 10:52:56 +0100

    add router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { BorderCardDirective } from "./border-card.directive";
 import { DetailPokemonComponent } from "./detail-pokemon/detail-pokemon.component";
 import { ListPokemonComponent } from "./list-pokemon/list-pokemon.component";
 import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,7 @@ import { PokemonTypeColorPipe } from "./pokemon-type-color.pipe";
     ListPokemonComponent,
     PokemonTypeColorPipe,
   ],
-  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
