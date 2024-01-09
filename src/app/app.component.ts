70a07010345a760d212859fdc4f0adc603452ddd
Lilian Mahut <lilian.mahut@pm.me>
2024-01-09 17:24:13 +0100

    refactoring des composants

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,7 @@
-import { Component, OnInit, TemplateRef } from "@angular/core";
-import { POKEMONS } from "./mock-pokemon-list";
-import { Pokemon } from "./pokemon";
-import { NgIfContext } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
-  standalone: false,
   templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
 })
-export class AppComponent {
-  pokemonList: Pokemon[] = POKEMONS;
-  pokemonSelected: Pokemon | undefined;
-  monPokemon: TemplateRef<NgIfContext<Pokemon | undefined>> | null;
-  aucunPokemon: TemplateRef<NgIfContext<Pokemon | undefined>> | null;
-
-  selectPokemon(pokemonId: string) {
-    const pokemon: Pokemon | undefined = this.pokemonList.find(
-      (pokemon) => pokemon.id == +pokemonId
-    );
-    if (pokemon) {
-      this.pokemonSelected = pokemon;
-    } else {
-      this.pokemonSelected = pokemon;
-    }
-  }
-}
+export class AppComponent {}
